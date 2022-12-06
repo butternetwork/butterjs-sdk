53d78367d64568063933e753435ce4760b9d224e
bakuful <dubakufulco@gmail.com>
2022-12-05 22:38:28 -0500

    change demo

--- test/demo/demo.ts
+++ test/demo/demo.ts
@@ -50,16 +50,19 @@ import { parseNearAmount } from 'near-api-js/lib/utils/format';
 import { asciiToHex, verifyNearAccountId } from '../../src/utils';
 import BN from 'bn.js';
 require('dotenv/config');
+
+// web3.js config
 const web3 = new Web3(
   'https://testnet-rpc.maplabs.io'
   // 'https://rpc.ankr.com/bsc_testnet_chapel/9a12629301614050e76136dcaf9627f5ef215f86fb1185d908f9d232b8530ef7'
 );
 const account = web3.eth.accounts.privateKeyToAccount(
-  '0x' + 'b87b1f26c7d0ffe0f65c25dbc09602e0ac9c0d14acc979b5d67439cade6cdb7b'
+  '0x' + process.env.EVM_PRIVATE_KEY
 );
 
 web3.eth.accounts.wallet.add(account);
 web3.eth.defaultAccount = account.address;
+
 // NEAR Network 配置 等同于ethers.js的signer, 如果src chain是Near需要配置
 const keyStore: InMemoryKeyStore = new keyStores.InMemoryKeyStore();
 const keyPair: KeyPair = KeyPair.fromString(process.env.NEAR_PRIVATE_KEY!);
@@ -71,6 +74,7 @@ const nearConfig = new NearNetworkConfig(
   'testnet'
 );
 
+// signer and providers
 const bscProvider = new ethers.providers.JsonRpcProvider(
   BSC_TEST_CHAIN.rpc,
   Number.parseInt(BSC_TEST_CHAIN.chainId)
@@ -82,84 +86,77 @@ const mapProvider = new ethers.providers.JsonRpcProvider(
   Number.parseInt(MAP_TEST_CHAIN.chainId)
 );
 const mapSigner = new ethers.Wallet(process.env.EVM_PRIVATE_KEY!, mapProvider);
-/** 支持的链 {@link ChainId} 调试中仅支持MAP测试网，ETH私链，和Near测试网**/
+
+/** 支持的链 {@link ChainId} 调试中仅支持MAP测试网，BSC测试，和Near测试网**/
 console.log('supported chain', SUPPORTED_CHAIN_LIST);
 /** 支持的token {@link supported_token.ts} **/
-console.log('supported token', ID_TO_SUPPORTED_TOKEN(ChainId.NEAR_TESTNET));
+console.log(
+  'supported token by chainId',
+  ID_TO_SUPPORTED_TOKEN(ChainId.NEAR_TESTNET)
+);
 
-/** 下面假设我们要从讲Near代币从ETH链Bridge到NEAR链从而获得NEAR上的原生Near代币
+/** 下面假设我们要从将MOST代币从BSC链Bridge到NEAR链从而获得NEAR上的MOST代币
  *  整个过程需要3个接口
  *    1. getBridgeFee
  *    2. getVaultBalance
  *    3. bridgeToken
  *  */
-function test(): PromiEvent<TransactionReceipt> {
-  return web3.eth.sendTransaction({
-    from: '0x8c9b3cAf7DedD3003f53312779c1b92ba1625D94',
-    to: '0xCBdb1Da4f99276b0c427776BDE93838Bc19386Cc',
-    value: '10000000',
-    gas: '5000000',
-    gasPrice: '100',
-  });
-}
+// function test(): PromiEvent<TransactionReceipt> {
+//   return web3.eth.sendTransaction({
+//     from: '0x8c9b3cAf7DedD3003f53312779c1b92ba1625D94',
+//     to: '0xCBdb1Da4f99276b0c427776BDE93838Bc19386Cc',
+//     value: '10000000',
+//     gas: '5000000',
+//     gasPrice: '100',
+//   });
+// }
 
-console.log(typeof nearConfig);
-const address = MOS_CONTRACT_ADDRESS_SET[ID_TO_CHAIN_ID('212')];
-console.log('mcs address', address);
 async function demo() {
   console.log('start demo');
-  const nearAccountState = await verifyNearAccountId(
-    'xyli.testnet',
-    ChainId.NEAR_TESTNET
-  );
-  console.log(nearAccountState);
 
   const provider: ButterJsonRpcProvider = {
     url: 'https://testnet-rpc.maplabs.io',
     chainId: 212,
   };
-  // 1. 获取费用信息
-  const fee: ButterFee = await getBridgeFee(
-    MAP_TEST_NATIVE,
-    ChainId.BSC_TEST,
-    ethers.utils.parseEther('1').mul(1).toString(),
-    provider
-  );
-  console.log('bridge fee', fee);
 
-  console.log('rate', await getDistributeRate(ChainId.MAP_TEST));
-  //
-  // // 2. 获取目标链的vault余额， 如果用户提供的数额大于余额应提示用户
-  const balance: VaultBalance = await getVaultBalance(
-    ChainId.NEAR_TESTNET,
-    NEAR_TEST_MOST,
-    ChainId.MAP_TEST,
-    provider
-  );
-  console.log('from token', MAP_TEST_NATIVE);
-  console.log('vault balance', balance);
-  //
-  // // 3. 获取targetToken
-  const now = Date.now();
+  // 获取token 从bsc链可以bridge到near链的token列表
   const tokenCandidates = await getTokenCandidates(
     ChainId.BSC_TEST,
-    ChainId.MAP_TEST,
+    ChainId.NEAR_TESTNET,
     {
       url: 'https://testnet-rpc.maplabs.io',
       chainId: 212,
     }
   );
-  console.log('token candidates', tokenCandidates, Date.now() - now);
+  console.log('token candidates', tokenCandidates);
 
-  // // 3. Bridge(先estimate gas)
+  // 1. 获取费用信息
+  const fee: ButterFee = await getBridgeFee(
+    BSC_TEST_MOST,
+    ChainId.NEAR_TESTNET,
+    ethers.utils.parseEther('1').toString(),
+    provider
+  );
+  console.log('bridge fee', fee);
+
+  // 2. 获取目标链的vault余额， 如果用户提供的数额大于余额应提示用户
+  const balance: VaultBalance = await getVaultBalance(
+    ChainId.BSC_TEST,
+    BSC_TEST_MOST,
+    ChainId.NEAR_TESTNET,
+    provider
+  );
+  console.log('vault balance', balance);
+
+  // 3. Bridge(先estimate gas)
   console.log('gas estimate');
   const bridge: ButterBridge = new ButterBridge();
   const request: BridgeRequestParam = {
     fromAddress: '0x8c9b3cAf7DedD3003f53312779c1b92ba1625D94',
-    fromToken: BSC_TEST_NATIVE,
+    fromToken: BSC_TEST_MOST,
     fromChainId: ChainId.BSC_TEST,
-    toChainId: ChainId.MAP_TEST,
-    toAddress: '0x8c9b3cAf7DedD3003f53312779c1b92ba1625D94',
+    toChainId: ChainId.NEAR_TESTNET,
+    toAddress: 'xyli.testnet',
     amount: ethers.utils.parseEther('1')!.toString(),
     options: { signerOrProvider: web3.eth },
   };
@@ -172,20 +169,20 @@ async function demo() {
 
   // 3. Bridge(真正的Bridge)
   const bridgeRequest: BridgeRequestParam = {
-    fromAddress: 'xyli.testnet',
-    fromToken: NEAR_TEST_MOST,
-    fromChainId: ChainId.NEAR_TESTNET,
-    toChainId: ChainId.MAP_TEST,
-    toAddress: '0x8c9b3cAf7DedD3003f53312779c1b92ba1625D94',
-    amount: ethers.utils.parseEther('1000000')!.toString(),
-    // amount: parseNearAmount('5')!.toString(),
+    fromAddress: '0x8c9b3cAf7DedD3003f53312779c1b92ba1625D94',
+    fromToken: BSC_TEST_MOST,
+    fromChainId: ChainId.BSC_TEST,
+    toChainId: ChainId.NEAR_TESTNET,
+    toAddress: 'xyli.testnet',
+    amount: ethers.utils.parseEther('1')!.toString(),
     options: {
       nearProvider: nearConfig,
-      signerOrProvider: mapSigner,
-      gas: '100000000000000',
-      // gas: adjustedGas,
+      signerOrProvider: bscSigner,
+      // gas: '100000000000000',
+      gas: adjustedGas,
     },
   };
+
   const response: ButterTransactionResponse = await bridge.bridgeToken(
     bridgeRequest
   );
