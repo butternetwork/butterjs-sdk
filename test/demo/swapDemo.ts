19562045689af6287d914f4d7db88b24be695856
bakuful <dubakufulco@gmail.com>
2023-01-09 10:52:14 -0500

    upgrade

--- test/demo/swapDemo.ts
+++ test/demo/swapDemo.ts
@@ -80,9 +80,9 @@ async function demo() {
 
   const fromAddress = '0x9f477490Aac940cE48249D8C455D8f6AE6Dc29c0';
   const toAddress = '0x9f477490Aac940cE48249D8C455D8f6AE6Dc29c0';
-  const fromToken = POLYGON_TEST_NATIVE;
+  const fromToken = POLYGON_TEST_USDC;
   const toToken = BSC_TEST_NATIVE;
-  const inputAmount = '0.01';
+  const inputAmount = '1';
 
   let signer;
   const fromChainId = fromToken.chainId;
