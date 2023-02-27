4e8832357f694bf68f1fe941c0777d019abc9e0a
= <=>
2023-02-27 16:24:14 +0800

    upgrade mos address

--- src/utils/addressUtil.ts
+++ src/utils/addressUtil.ts
@@ -20,7 +20,6 @@ export function validateAndParseAddressByChainId(
   address: string,
   chainId: string
 ): string {
-  console.log('test')
   if (IS_EVM(chainId)) {
     try {
       return getAddress(address);
