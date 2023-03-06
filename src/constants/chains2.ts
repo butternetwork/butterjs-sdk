7356719825f9edd416e46b194b674ec05832f2d0
ivan.macaron <ivan.macaron>
2023-03-06 11:47:16 +0800

    upgrade address

--- src/constants/chains2.ts
+++ src/constants/chains2.ts
@@ -1,27 +1,27 @@
 import {Chain, Currency} from "../beans";
 import {
-    CHAIN_ID as CID, CHAIN_NAME as CNM, TOKEN_ID as TID,
+    CHAIN_ID, CHAIN_NAME, TOKEN_ID,
     TOKENS_BNB, TOKENS_BNB_TEST, TOKENS_GOERLI, TOKENS_MAP, TOKENS_MAP_TEST,
     TOKENS_NEAR, TOKENS_NEAR_TEST, TOKENS_POLYGON, TOKENS_POLYGON_TEST, TOKENS_PRIV
 } from "./config";
 
 export {CHAIN_ID, CHAIN_NAME, TOKEN_ID} from './config';
 
-const TOKEN_PRIV = (tokenId: TID | string): Currency => {
+const TOKEN_PRIV = (tokenId: TOKEN_ID | string): Currency => {
     let current: any = TOKENS_PRIV[tokenId];
     if (current) {
         return ((current as Currency)).copy;
     }
     throw new Error(`Not Support this Token(${tokenId}) in ETH Priv Chain`)
 }
-const TOKEN_GOERLI = (tokenId: TID | string): Currency => {
+const TOKEN_GOERLI = (tokenId: TOKEN_ID | string): Currency => {
     let current: any = TOKENS_GOERLI[tokenId];
     if (current) {
         return ((current as Currency)).copy;
     }
     throw new Error(`Not Support this Token(${tokenId}) in ETH Goerli Chain`)
 }
-const TOKEN_MAP = (tokenId: TID | string, isTest = true): Currency => {
+const TOKEN_MAP = (tokenId: TOKEN_ID | string, isTest = true): Currency => {
     let current: any;
     if (isTest) {
         current = TOKENS_MAP_TEST[tokenId];
@@ -33,7 +33,7 @@ const TOKEN_MAP = (tokenId: TID | string, isTest = true): Currency => {
     }
     throw new Error(`Not Support this Token(${tokenId}) in MAP | ${isTest}`)
 }
-const TOKEN_BNB = (tokenId: TID | string, isTest = true): Currency => {
+const TOKEN_BNB = (tokenId: TOKEN_ID | string, isTest = true): Currency => {
     let current: any;
     if (isTest) {
         current = TOKENS_BNB_TEST[tokenId];
@@ -46,7 +46,7 @@ const TOKEN_BNB = (tokenId: TID | string, isTest = true): Currency => {
 
     throw new Error(`Not Support this Token(${tokenId}) in BNB Chain | ${isTest}`)
 }
-const TOKEN_POLYGON = (tokenId: TID | string, isTest = true): Currency => {
+const TOKEN_POLYGON = (tokenId: TOKEN_ID | string, isTest = true): Currency => {
     let current: any;
     if (isTest) {
         current = TOKENS_POLYGON_TEST[tokenId];
@@ -58,7 +58,7 @@ const TOKEN_POLYGON = (tokenId: TID | string, isTest = true): Currency => {
     }
     throw new Error(`Not Support this Token(${tokenId}) in Polygon Chain | ${isTest}`)
 }
-const TOKEN_NEAR = (tokenId: TID | string, isTest = true): Currency => {
+const TOKEN_NEAR = (tokenId: TOKEN_ID | string, isTest = true): Currency => {
     let current: any;
     if (isTest) {
         current = TOKENS_NEAR_TEST[tokenId];
@@ -72,147 +72,147 @@ const TOKEN_NEAR = (tokenId: TID | string, isTest = true): Currency => {
     throw new Error(`Not Support this Token(${tokenId}) in Near Chain | ${isTest}`)
 }
 
-export const TO_CID = (str: CID | CNM | string) => {
+export const TO_CHAIN_ID = (str: CHAIN_ID | CHAIN_NAME | string) => {
     switch (str) {
-        case CID.MAP_MAINNET:
-        case CNM.MAP_MAINNET:
-            return CID.MAP_MAINNET;
-        case CID.BNB_MAINNET:
-        case CNM.BNB_MAINNET:
-            return CID.BNB_MAINNET;
-        case CID.POLYGON_MAINNET:
-        case CNM.POLYGON_MAINNET:
-            return CID.POLYGON_MAINNET;
-        case CID.NEAR_MAINNET:
-        case CNM.NEAR_MAINNET:
-            return CID.NEAR_MAINNET;
-        case CID.ETH_PRIV:
-        case CNM.ETH_PRIV:
-            return CID.ETH_PRIV;
-        case CID.ETH_GOERLI:
-        case CNM.ETH_GOERLI:
-            return CID.ETH_GOERLI;
-        case CID.MAP_TEST:
-        case CNM.MAP_TEST:
-            return CID.MAP_TEST;
-        case CID.BNB_TEST:
-        case CNM.BNB_TEST:
-            return CID.BNB_TEST;
-        case CID.POLYGON_TEST:
-        case CNM.POLYGON_TEST:
-            return CID.POLYGON_TEST;
-        case CID.NEAR_TEST:
-        case CNM.NEAR_TEST:
-            return CID.NEAR_TEST;
+        case CHAIN_ID.MAP_MAINNET:
+        case CHAIN_NAME.MAP_MAINNET:
+            return CHAIN_ID.MAP_MAINNET;
+        case CHAIN_ID.BNB_MAINNET:
+        case CHAIN_NAME.BNB_MAINNET:
+            return CHAIN_ID.BNB_MAINNET;
+        case CHAIN_ID.POLYGON_MAINNET:
+        case CHAIN_NAME.POLYGON_MAINNET:
+            return CHAIN_ID.POLYGON_MAINNET;
+        case CHAIN_ID.NEAR_MAINNET:
+        case CHAIN_NAME.NEAR_MAINNET:
+            return CHAIN_ID.NEAR_MAINNET;
+        case CHAIN_ID.ETH_PRIV:
+        case CHAIN_NAME.ETH_PRIV:
+            return CHAIN_ID.ETH_PRIV;
+        case CHAIN_ID.ETH_GOERLI:
+        case CHAIN_NAME.ETH_GOERLI:
+            return CHAIN_ID.ETH_GOERLI;
+        case CHAIN_ID.MAP_TEST:
+        case CHAIN_NAME.MAP_TEST:
+            return CHAIN_ID.MAP_TEST;
+        case CHAIN_ID.BNB_TEST:
+        case CHAIN_NAME.BNB_TEST:
+            return CHAIN_ID.BNB_TEST;
+        case CHAIN_ID.POLYGON_TEST:
+        case CHAIN_NAME.POLYGON_TEST:
+            return CHAIN_ID.POLYGON_TEST;
+        case CHAIN_ID.NEAR_TEST:
+        case CHAIN_NAME.NEAR_TEST:
+            return CHAIN_ID.NEAR_TEST;
     }
     throw new Error(
-        `TO_CID Unsupported network params: ${str}`
+        `TO_CHAIN_ID Unsupported network params: ${str}`
     );
 }
 
-export const CHAINS = (chainId: CID | string): Chain => {
-    if (chainId === CID.MAP_MAINNET) {
-        return new Chain(CID.MAP_MAINNET,
+export const CHAINS = (chainId: CHAIN_ID | string): Chain => {
+    if (chainId === CHAIN_ID.MAP_MAINNET) {
+        return new Chain(CHAIN_ID.MAP_MAINNET,
             'MAPO Mainnet', 'MAPO',
             'https://poc3-rpc.maplabs.io/',
             'https://makalu.mapscan.io/',
             'https://files.maplabs.io/bridge/map.png',
-            CNM.MAP_MAINNET,
-            TOKEN_MAP(TID.NATIVE, false),
+            CHAIN_NAME.MAP_MAINNET,
+            TOKEN_MAP(TOKEN_ID.NATIVE, false),
         );
     }
-    if (chainId === CID.BNB_MAINNET) {
-        return new Chain(CID.BNB_MAINNET,
+    if (chainId === CHAIN_ID.BNB_MAINNET) {
+        return new Chain(CHAIN_ID.BNB_MAINNET,
             'BNB Chain', 'BNB',
             'https://bsc-dataseed1.defibit.io/',
             'https://bscscan.com/',
             'https://uploads-ssl.webflow.com/62f34c32e8660c273054c17c/62fb88db22514137f2029167_bnb%20logo.png',
-            CNM.BNB_MAINNET,
-            TOKEN_BNB(TID.NATIVE, false),
+            CHAIN_NAME.BNB_MAINNET,
+            TOKEN_BNB(TOKEN_ID.NATIVE, false),
         );
     }
-    if (chainId === CID.POLYGON_MAINNET) {
-        return new Chain(CID.POLYGON_MAINNET,
+    if (chainId === CHAIN_ID.POLYGON_MAINNET) {
+        return new Chain(CHAIN_ID.POLYGON_MAINNET,
             'Polygon', 'MATIC',
             'https://polygon-rpc.com/',
             'https://polygonscan.com/',
             'https://cryptologos.cc/logos/polygon-matic-logo.png',
-            CNM.POLYGON_MAINNET,
-            TOKEN_POLYGON(TID.NATIVE, false),
+            CHAIN_NAME.POLYGON_MAINNET,
+            TOKEN_POLYGON(TOKEN_ID.NATIVE, false),
         );
     }
-    if (chainId === CID.NEAR_MAINNET) {
-        return new Chain(CID.NEAR_MAINNET,
+    if (chainId === CHAIN_ID.NEAR_MAINNET) {
+        return new Chain(CHAIN_ID.NEAR_MAINNET,
             'Near', 'NEAR',
             'https://rpc.mainnet.near.org',
             'https://explorer.near.org/',
             'https://cryptologos.cc/logos/near-protocol-near-logo.png',
-            CNM.NEAR_MAINNET,
-            TOKEN_NEAR(TID.NATIVE, false),
+            CHAIN_NAME.NEAR_MAINNET,
+            TOKEN_NEAR(TOKEN_ID.NATIVE, false),
         );
     }
-    if (chainId === CID.ETH_PRIV) {
-        return new Chain(CID.ETH_PRIV,
+    if (chainId === CHAIN_ID.ETH_PRIV) {
+        return new Chain(CHAIN_ID.ETH_PRIV,
             'Ethereum Private', 'ETH',
             'http://18.138.248.113:8545',
             '',
             'https://files.maplabs.io/bridge/eth.png',
-            CNM.ETH_PRIV,
-            TOKEN_PRIV(TID.NATIVE),
+            CHAIN_NAME.ETH_PRIV,
+            TOKEN_PRIV(TOKEN_ID.NATIVE),
         );
     }
-    if (chainId === CID.ETH_GOERLI) {
-        return new Chain(CID.ETH_GOERLI,
+    if (chainId === CHAIN_ID.ETH_GOERLI) {
+        return new Chain(CHAIN_ID.ETH_GOERLI,
             'Ethereum Goerli', 'ETH',
             'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
             'https://goerli.etherscan.io/',
             'https://files.maplabs.io/bridge/eth.png',
-            CNM.ETH_GOERLI,
-            TOKEN_GOERLI(TID.NATIVE),
+            CHAIN_NAME.ETH_GOERLI,
+            TOKEN_GOERLI(TOKEN_ID.NATIVE),
         );
     }
-    if (chainId === CID.MAP_TEST) {
+    if (chainId === CHAIN_ID.MAP_TEST) {
         return new Chain(
-            CID.MAP_TEST,
+            CHAIN_ID.MAP_TEST,
             'MAPO Testnet', 'MAPO',
             'https://testnet-rpc.maplabs.io',
             'http://18.139.224.21:9001/',
             'https://files.maplabs.io/bridge/map.png',
-            CNM.MAP_TEST,
-            TOKEN_MAP(TID.NATIVE),
+            CHAIN_NAME.MAP_TEST,
+            TOKEN_MAP(TOKEN_ID.NATIVE),
         );
     }
-    if (chainId === CID.BNB_TEST) {
+    if (chainId === CHAIN_ID.BNB_TEST) {
         return new Chain(
-            CID.BNB_TEST,
+            CHAIN_ID.BNB_TEST,
             'BNB Chain Testnet', 'BNB',
             'https://rpc.ankr.com/bsc_testnet_chapel/9a12629301614050e76136dcaf9627f5ef215f86fb1185d908f9d232b8530ef7',
             'https://testnet.bscscan.com/',
             'https://uploads-ssl.webflow.com/62f34c32e8660c273054c17c/62fb88db22514137f2029167_bnb%20logo.png',
-            CNM.BNB_TEST,
-            TOKEN_BNB(TID.NATIVE),
+            CHAIN_NAME.BNB_TEST,
+            TOKEN_BNB(TOKEN_ID.NATIVE),
         );
     }
-    if (chainId === CID.POLYGON_TEST) {
+    if (chainId === CHAIN_ID.POLYGON_TEST) {
         return new Chain(
-            CID.POLYGON_TEST,
+            CHAIN_ID.POLYGON_TEST,
             'Polygon Testnet', 'MATIC',
             'https://rpc-mumbai.maticvigil.com/',
             'https://mumbai.polygonscan.com/',
             'https://cryptologos.cc/logos/polygon-matic-logo.png',
-            CNM.POLYGON_TEST,
-            TOKEN_POLYGON(TID.NATIVE),
+            CHAIN_NAME.POLYGON_TEST,
+            TOKEN_POLYGON(TOKEN_ID.NATIVE),
         );
     }
-    if (chainId === CID.NEAR_TEST) {
+    if (chainId === CHAIN_ID.NEAR_TEST) {
         return new Chain(
-            CID.NEAR_TEST,
+            CHAIN_ID.NEAR_TEST,
             'Near Testnet', 'NEAR',
             'https://rpc.testnet.near.org',
             'https://explorer.testnet.near.org/',
             'https://cryptologos.cc/logos/near-protocol-near-logo.png',
-            CNM.NEAR_TEST,
-            TOKEN_NEAR(TID.NATIVE),
+            CHAIN_NAME.NEAR_TEST,
+            TOKEN_NEAR(TOKEN_ID.NATIVE),
         );
     }
     throw new Error(
@@ -221,35 +221,35 @@ export const CHAINS = (chainId: CID | string): Chain => {
 
 }
 
-export const TOKENS = (chainId: CID | string, tokenId: TID | string): Currency | null => {
-    if (chainId === CID.MAP_MAINNET) {
+export const TOKENS = (chainId: CHAIN_ID | string, tokenId: TOKEN_ID | string): Currency | null => {
+    if (chainId === CHAIN_ID.MAP_MAINNET) {
         return TOKEN_MAP(tokenId, false);
     }
-    if (chainId === CID.BNB_MAINNET) {
+    if (chainId === CHAIN_ID.BNB_MAINNET) {
         return TOKEN_BNB(tokenId, false);
     }
-    if (chainId === CID.POLYGON_MAINNET) {
+    if (chainId === CHAIN_ID.POLYGON_MAINNET) {
         return TOKEN_POLYGON(tokenId, false);
     }
-    if (chainId === CID.NEAR_MAINNET) {
+    if (chainId === CHAIN_ID.NEAR_MAINNET) {
         return TOKEN_NEAR(tokenId, false);
     }
-    if (chainId === CID.ETH_PRIV) {
+    if (chainId === CHAIN_ID.ETH_PRIV) {
         return TOKEN_PRIV(tokenId);
     }
-    if (chainId === CID.ETH_GOERLI) {
+    if (chainId === CHAIN_ID.ETH_GOERLI) {
         return TOKEN_GOERLI(tokenId);
     }
-    if (chainId === CID.MAP_TEST) {
+    if (chainId === CHAIN_ID.MAP_TEST) {
         return TOKEN_MAP(tokenId);
     }
-    if (chainId === CID.BNB_TEST) {
+    if (chainId === CHAIN_ID.BNB_TEST) {
         return TOKEN_BNB(tokenId);
     }
-    if (chainId === CID.POLYGON_TEST) {
+    if (chainId === CHAIN_ID.POLYGON_TEST) {
         return TOKEN_POLYGON(tokenId);
     }
-    if (chainId === CID.NEAR_TEST) {
+    if (chainId === CHAIN_ID.NEAR_TEST) {
         return TOKEN_NEAR(tokenId);
     }
     throw new Error(
@@ -262,63 +262,63 @@ export const TOKENS = (chainId: CID | string, tokenId: TID | string): Currency |
  * @param chainId
  * @constructor
  */
-export const AVAILABLE_TOKENS = (chainId: CID | string): Currency[] => {
+export const AVAILABLE_TOKENS = (chainId: CHAIN_ID | string): Currency[] => {
     let items = [];
-    if (chainId === CID.MAP_MAINNET) {
+    if (chainId === CHAIN_ID.MAP_MAINNET) {
         for (const key in TOKENS_MAP) {
             items.push(TOKEN_MAP(key, false));
         }
         return items;
     }
-    if (chainId === CID.BNB_MAINNET) {
+    if (chainId === CHAIN_ID.BNB_MAINNET) {
         for (const key in TOKENS_BNB) {
             items.push(TOKEN_BNB(key, false));
         }
         return items;
     }
-    if (chainId === CID.POLYGON_MAINNET) {
+    if (chainId === CHAIN_ID.POLYGON_MAINNET) {
         for (const key in TOKENS_POLYGON) {
             items.push(TOKEN_POLYGON(key, false));
         }
         return items;
     }
-    if (chainId === CID.NEAR_MAINNET) {
+    if (chainId === CHAIN_ID.NEAR_MAINNET) {
         for (const key in TOKENS_NEAR) {
             items.push(TOKEN_NEAR(key, false));
         }
         return items;
     }
-    if (chainId === CID.ETH_PRIV) {
+    if (chainId === CHAIN_ID.ETH_PRIV) {
         for (const key in TOKENS_PRIV) {
             items.push(TOKEN_PRIV(key));
         }
         return items;
     }
-    if (chainId === CID.ETH_GOERLI) {
+    if (chainId === CHAIN_ID.ETH_GOERLI) {
         for (const key in TOKENS_GOERLI) {
             items.push(TOKEN_GOERLI(key));
         }
         return items;
     }
-    if (chainId === CID.MAP_TEST) {
+    if (chainId === CHAIN_ID.MAP_TEST) {
         for (const key in TOKENS_MAP_TEST) {
             items.push(TOKEN_MAP(key, true));
         }
         return items;
     }
-    if (chainId === CID.BNB_TEST) {
+    if (chainId === CHAIN_ID.BNB_TEST) {
         for (const key in TOKENS_BNB_TEST) {
             items.push(TOKEN_BNB(key, true));
         }
         return items;
     }
-    if (chainId === CID.POLYGON_TEST) {
+    if (chainId === CHAIN_ID.POLYGON_TEST) {
         for (const key in TOKENS_POLYGON_TEST) {
             items.push(TOKEN_POLYGON(key, true));
         }
         return items;
     }
-    if (chainId === CID.NEAR_TEST) {
+    if (chainId === CHAIN_ID.NEAR_TEST) {
         for (const key in TOKENS_NEAR_TEST) {
             items.push(TOKEN_NEAR(key, true));
         }
@@ -331,59 +331,59 @@ export const AVAILABLE_TOKENS = (chainId: CID | string): Currency[] => {
  * @param chainId
  * @constructor
  */
-export const SUPPORT_TOKENS = (chainId: CID | string): Currency[] => {
-    if (chainId === CID.MAP_MAINNET) {
+export const SUPPORT_TOKENS = (chainId: CHAIN_ID | string): Currency[] => {
+    if (chainId === CHAIN_ID.MAP_MAINNET) {
         return [];
     }
-    if (chainId === CID.BNB_MAINNET) {
-        return [TOKEN_BNB(TID.USDC, false)];
+    if (chainId === CHAIN_ID.BNB_MAINNET) {
+        return [TOKEN_BNB(TOKEN_ID.USDC, false)];
     }
-    if (chainId === CID.POLYGON_MAINNET) {
-        return [TOKEN_POLYGON(TID.USDC, false)];
+    if (chainId === CHAIN_ID.POLYGON_MAINNET) {
+        return [TOKEN_POLYGON(TOKEN_ID.USDC, false)];
     }
-    if (chainId === CID.NEAR_MAINNET) {
-        return [TOKEN_NEAR(TID.USDC, false)];
+    if (chainId === CHAIN_ID.NEAR_MAINNET) {
+        return [TOKEN_NEAR(TOKEN_ID.USDC, false)];
     }
-    if (chainId === CID.ETH_PRIV) {
+    if (chainId === CHAIN_ID.ETH_PRIV) {
         return [];
     }
-    if (chainId === CID.ETH_GOERLI) {
+    if (chainId === CHAIN_ID.ETH_GOERLI) {
         return [
-            TOKEN_GOERLI(TID.NATIVE),
-            TOKEN_GOERLI(TID.USDC)
+            TOKEN_GOERLI(TOKEN_ID.NATIVE),
+            TOKEN_GOERLI(TOKEN_ID.USDC)
         ];
     }
-    if (chainId === CID.MAP_TEST) {
+    if (chainId === CHAIN_ID.MAP_TEST) {
         return [
-            TOKEN_MAP(TID.USDC),
+            TOKEN_MAP(TOKEN_ID.USDC),
         ];
     }
-    if (chainId === CID.BNB_TEST) {
+    if (chainId === CHAIN_ID.BNB_TEST) {
         return [
-            TOKEN_BNB(TID.NATIVE),
-            TOKEN_BNB(TID.USDC),
-            TOKEN_BNB(TID.BMOS),
+            TOKEN_BNB(TOKEN_ID.NATIVE),
+            TOKEN_BNB(TOKEN_ID.USDC),
+            TOKEN_BNB(TOKEN_ID.BMOS),
         ];
     }
-    if (chainId === CID.POLYGON_TEST) {
+    if (chainId === CHAIN_ID.POLYGON_TEST) {
         return [
-            TOKEN_POLYGON(TID.NATIVE),
-            TOKEN_POLYGON(TID.USDC),
-            TOKEN_POLYGON(TID.BMOS),
+            TOKEN_POLYGON(TOKEN_ID.NATIVE),
+            TOKEN_POLYGON(TOKEN_ID.USDC),
+            TOKEN_POLYGON(TOKEN_ID.BMOS),
         ];
     }
-    if (chainId === CID.NEAR_TEST) {
+    if (chainId === CHAIN_ID.NEAR_TEST) {
         return [
-            TOKEN_NEAR(TID.NATIVE),
-            TOKEN_NEAR(TID.USDC)
+            TOKEN_NEAR(TOKEN_ID.NATIVE),
+            TOKEN_NEAR(TOKEN_ID.USDC)
         ];
     }
     throw new Error(`SUPPORT_TOKENS: unknown chain id: ${chainId}`);
 };
 
-export const NEAR_CONNECT = (chainId: CID | string) => {
+export const NEAR_CONNECT = (chainId: CHAIN_ID | string) => {
     const connectionConfig = {
-        networkId: chainId === CID.NEAR_TEST ? 'testnet' : '',
+        networkId: chainId === CHAIN_ID.NEAR_TEST ? 'testnet' : '',
         nodeUrl: CHAINS(chainId).rpc,
     };
     if (!connectionConfig.networkId) {
@@ -396,8 +396,8 @@ export const NEAR_CONNECT = (chainId: CID | string) => {
 
 export const IS_MAP = (id: string): boolean => {
     switch (id) {
-        case CID.MAP_MAINNET:
-        case CID.MAP_TEST:
+        case CHAIN_ID.MAP_MAINNET:
+        case CHAIN_ID.MAP_TEST:
             return true;
         default:
             return false;
@@ -406,8 +406,8 @@ export const IS_MAP = (id: string): boolean => {
 
 export const IS_NEAR = (id: string): boolean => {
     switch (id) {
-        case CID.NEAR_TEST:
-        case CID.NEAR_MAINNET:
+        case CHAIN_ID.NEAR_TEST:
+        case CHAIN_ID.NEAR_MAINNET:
             return true;
         default:
             return false;
@@ -417,14 +417,14 @@ export const IS_NEAR = (id: string): boolean => {
 export const IS_EVM = (id: string): boolean => {
     switch (id) {
         case '1':
-        case CID.POLYGON_MAINNET:
-        case CID.BNB_MAINNET:
-        case CID.MAP_MAINNET:
-        case CID.BNB_TEST:
-        case CID.POLYGON_TEST:
-        case CID.MAP_TEST:
-        case CID.ETH_GOERLI:
-        case CID.ETH_PRIV:
+        case CHAIN_ID.POLYGON_MAINNET:
+        case CHAIN_ID.BNB_MAINNET:
+        case CHAIN_ID.MAP_MAINNET:
+        case CHAIN_ID.BNB_TEST:
+        case CHAIN_ID.POLYGON_TEST:
+        case CHAIN_ID.MAP_TEST:
+        case CHAIN_ID.ETH_GOERLI:
+        case CHAIN_ID.ETH_PRIV:
             return true;
         default:
             return false;
@@ -434,10 +434,10 @@ export const IS_EVM = (id: string): boolean => {
 export const IS_MAINNET = (id: string): boolean => {
     switch (id) {
         case '1':
-        case CID.MAP_MAINNET:
-        case CID.BNB_MAINNET:
-        case CID.POLYGON_MAINNET:
-        case CID.NEAR_MAINNET:
+        case CHAIN_ID.MAP_MAINNET:
+        case CHAIN_ID.BNB_MAINNET:
+        case CHAIN_ID.POLYGON_MAINNET:
+        case CHAIN_ID.NEAR_MAINNET:
             return true;
         default:
             return false;
