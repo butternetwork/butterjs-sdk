29a217eddf22033f8d89eee6038c60622e51eeb9
Ivan <ivanmacaron>
2023-03-15 18:08:28 +0800

    add main net config

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,5 @@
 export const DEFAULT_SLIPPAGE = 200;
 export const NEAR_TOKEN_SEPARATOR = 'X';
 export const BUTTER_SMART_ROUTER_URL = 'https://testnet-api.butterswap.io/router/best_route';
+// export const BUTTER_SMART_ROUTER_URL_MAINNET = 'http://api.butterswap.io/router/best_route';
 export const BUTTER_SMART_ROUTER_URL_MAINNET = 'http://127.0.0.1:9009/router/best_route';
